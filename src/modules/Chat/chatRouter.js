bc32c41c08008855b94b12241dc810229050c10f
Yomna Elshorbagy <yumnamohamed30@gmail.com>
2025-12-20 13:04:11 +0200

    update chat ai router

--- src/modules/Chat/chatRouter.js
+++ src/modules/Chat/chatRouter.js
@@ -7,31 +7,32 @@ import axios from "axios";
 import { analyzePetImage } from "./chatAi.js";
 
 const chatRouter = Router();
-const uploadImage = multer({
-  storage: multer.memoryStorage(),
-});
-chatRouter.post("/disease", uploadImage.single("image"), async (req, res) => {
-  try {
-    if (!req.file) return res.status(400).json({ message: "No file uploaded" });
 
-    const form = new FormData();
-    form.append("image", req.file.buffer, {
-      filename: req.file.originalname,
-      contentType: req.file.mimetype,
-    });
+// const uploadImage = multer({
+//   storage: multer.memoryStorage(),
+// });
+// chatRouter.post("/disease", uploadImage.single("image"), async (req, res) => {
+//   try {
+//     if (!req.file) return res.status(400).json({ message: "No file uploaded" });
 
-    const flaskResponse = await axios.post(process.env.FLASK_URL, form, {
-      headers: form.getHeaders(),
-    });
+//     const form = new FormData();
+//     form.append("image", req.file.buffer, {
+//       filename: req.file.originalname,
+//       contentType: req.file.mimetype,
+//     });
 
-    res.json(flaskResponse.data);
-  } catch (err) {
-    res.status(500).json({
-      message: "Error forwarding request to Flask",
-      error: err.message,
-    });
-  }
-});
+//     const flaskResponse = await axios.post(process.env.FLASK_URL, form, {
+//       headers: form.getHeaders(),
+//     });
+
+//     res.json(flaskResponse.data);
+//   } catch (err) {
+//     res.status(500).json({
+//       message: "Error forwarding request to Flask",
+//       error: err.message,
+//     });
+//   }
+// });
 
 chatRouter.post("/analyze-image", async (req, res) => {
   try {
